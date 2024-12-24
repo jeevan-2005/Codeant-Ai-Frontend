f880d762a012c3d664b8b932ccbc11064ee8cd78
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-12-24 15:28:04 +0530

    settings page created.

---
+++ src/Components/Settings/Settings.jsx
@@ -0,0 +1,13 @@
+import SideBar from '../SideBar'
+import NotFound from '../NotFound'
+
+const Settings = () => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <SideBar activeInd={4} />
+      <NotFound />
+    </div>
+  )
+}
+
+export default Settings;
