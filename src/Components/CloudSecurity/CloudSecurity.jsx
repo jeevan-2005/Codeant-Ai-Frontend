382fd83e82913e97703e35fc6a090afb3fdf2c58
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-12-24 15:27:50 +0530

    cloud security page created.

---
+++ src/Components/CloudSecurity/CloudSecurity.jsx
@@ -0,0 +1,13 @@
+import NotFound from '../NotFound'
+import SideBar from '../SideBar'
+
+const CloudSecurity = () => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <SideBar activeInd={2} />
+      <NotFound />
+    </div>
+  )
+}
+
+export default CloudSecurity
