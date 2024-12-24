3b42038099adc02b98fed5ee30fa48bed541fe55
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-12-24 15:28:30 +0530

    Guide page created.

---
+++ src/Components/Guide/Guide.jsx
@@ -0,0 +1,13 @@
+import SideBar from '../SideBar'
+import NotFound from '../NotFound'
+
+const Guide = () => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <SideBar activeInd={3} />
+      <NotFound />
+    </div>
+  )
+}
+
+export default Guide
