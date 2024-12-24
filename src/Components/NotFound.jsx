918f0bf5f224f3a0f423e7c6bfaa1d9eaaae521f
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-12-24 15:34:33 +0530

    nothing found page created.

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="bg-[#F7F5F5] h-screen md:w-[calc(100%-242px)] md:mt-0 w-full md:p-3">
+      <div className="bg-white w-full h-full rounded-xl flex justify-center items-center">
+        <p className="text-[#181D27] font-semibold text-[22px] p-5">Nothing is found</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
