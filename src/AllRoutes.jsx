98f060ec9f263ae97be7ffe317c8408592743f0e
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-12-24 15:52:57 +0530

    modified the page

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,24 +1,24 @@
-import {Route, Routes} from "react-router-dom"
-import SignUp from "./Components/SignUpPage/SignUp"
-import RepoPage from "./Components/RepositoriesPage/RepoPage"
-import AiCodeReviewPage from "./Components/AICodeReviewPage/AiCodeReviewPage"
-import CloudSecurity from "./Components/CloudSecurity/CloudSecurity"
-import Guide from "./Components/Guide/Guide"
-import Settings from "./Components/Settings/Settings"
-import Support from "./Components/Support/Support"
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./Components/SignUpPage/SignUp";
+import RepoPage from "./Components/RepositoriesPage/RepoPage";
+import AiCodeReviewPage from "./Components/AICodeReviewPage/AiCodeReviewPage";
+import CloudSecurity from "./Components/CloudSecurity/CloudSecurity";
+import Guide from "./Components/Guide/Guide";
+import Settings from "./Components/Settings/Settings";
+import Support from "./Components/Support/Support";
 
 const AllRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/repositories" element={<RepoPage />} />
-        <Route path="/ai-code-review" element={<AiCodeReviewPage />} />
-        <Route path="/cloud-security" element={<CloudSecurity />} />
-        <Route path="/guide" element={<Guide />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/support" element={<Support />} />
+      <Route path="/" element={<SignUp />} />
+      <Route path="/repositories" element={<RepoPage />} />
+      <Route path="/ai-code-review" element={<AiCodeReviewPage />} />
+      <Route path="/cloud-security" element={<CloudSecurity />} />
+      <Route path="/guide" element={<Guide />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/support" element={<Support />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
